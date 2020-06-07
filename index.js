58aae528275ef68c49093d0bdbd5e50be90046dc
Faisal Arshed <faisal.arshed.28@gmail.com>
2020-06-07 10:40:41 +0500

    Update funcs

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-exports.searchLyrics = require('./lib/SearchLyrics');
-exports.getLyricsFromPath = require('./lib/GetLyricsFromPath');
-exports.getAlbumArtFromPath = require('./lib/GetAlbumArtFromPath');
 exports.getLyrics = require('./lib/GetLyrics');
-exports.getAlbumArt = require('./lib/GetAlbumArt');
-exports.getSong = require('./lib/GetSong');
+exports.getLyricsFromPath = require('./lib/GetLyricsFromPath');
+exports.searchLyrics = require('./lib/SearchLyrics');
