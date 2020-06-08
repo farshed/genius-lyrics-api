50a7a750573c9194dec32dfb4a0661909f0228e0
Faisal Arshed <faisal.arshed.28@gmail.com>
2020-06-08 09:30:13 +0500

    cleanup

--- index.js
+++ index.js
@@ -2,12 +2,4 @@ exports.getLyrics = require('./lib/getLyrics');
 exports.getAlbumArt = require('./lib/getAlbumArt');
 exports.getSong = require('./lib/getSong');
 exports.searchSong = require('./lib/searchSong');
-// exports.getSongFromUrl = require('./lib/getSongFromId');
-
-const search = require('./lib/getSongFromId');
-const options = {
-	title: 'Blinding Lights',
-	artist: 'The Weeknd',
-	apiKey: 'xGUkuEImvLYnfrvUptFJGdbXeEee6Bn4ul8li5nwH7QNl3gETByg5N3GMgQEbHXg'
-};
-search(378195, options.apiKey);
+exports.getSongFromId = require('./lib/getSongFromId');
